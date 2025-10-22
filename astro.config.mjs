fa93d71704fe031e776c9332be3b0f0c9ed90a8c
Lscythe <lscythe.git@proton.me>
2025-10-22 08:54:33 +0700

    remove cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import cloudflare from "@astrojs/cloudflare";
 import { schema } from "./src/env";
 
 export default defineConfig({
   site: "https://rendrap.dev",
   integrations: [sitemap()],
-  adapter: cloudflare({
-    imageService: "cloudflare",
-  }),
-  server: { host: true },
   build: {
     format: "preserve",
     assets: "static",
   },
-  scopedStyleStrategy: "class",
   env: {
     schema,
     validateSecrets: true,
